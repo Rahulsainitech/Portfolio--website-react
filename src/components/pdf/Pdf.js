4810f27d6e257a9fb828e768ba644296947cab66
Rahulsainitech <rahuliim2001@gmail.com>
2023-07-24 13:48:39 +0530

    adding latest resume

--- src/components/pdf/Pdf.js
+++ src/components/pdf/Pdf.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import jspdf from 'jspdf';
-import r1 from '../../images/r1.jpg';
-import r2 from '../../images/r2.jpg';
+// import jspdf from 'jspdf';
+// import r123 from '../../images/r123.png';
+// import r2 from '../../images/r2.jpg';
+
 
 const Pdf = () => {
-  const pdfgenerate=()=>{
-    var doc=new jspdf("portrait","px","a4","false")
-    doc.addImage(r1,'PNG',65,20,500,400)
-    doc.addPage()
-    doc.addImage(r2,'PNG',65,20,500,400)
-    // doc.text(60,60,'Name')
-    // doc.text(60,80,'Email')
-    // doc.text(60,100,'Phone')
-    // doc.text(100,60,'Name')
-    // doc.text(100,60,'Name')
-    // doc.text(100,60,'Name')
-    doc.save("Rahul.pdf")
-  }
+  // const pdfgenerate=()=>{
+  //   var doc=new jspdf("portrait","px","a4","false")
+  //   doc.addImage(r123,'PNG',-27.8,0,650.63,842)
+  //   // doc.addPage()
+  //   // doc.addImage(r2,'PNG',65,20,500,400)
+  //   // doc.text(60,60,'Name')
+  //   // doc.text(60,80,'Email')
+  //   // doc.text(60,100,'Phone')
+  //   // doc.text(100,60,'Name')
+  //   // doc.text(100,60,'Name')
+  //   // doc.text(100,60,'Name')
+  //   doc.save("Rahul.pdf")
+  // }
   return (
-    <div><button className='btn btn-sm btn-outline-info m-3 py-2' onClick={()=>pdfgenerate()}>Download resume <i class="fas fa-download"/></button></div>
+    <a href="https://drive.google.com/file/d/1s0uC0YEH-zfxTYk-xcs0RdZ-0Ee5Nxs-/view?usp=sharing" target='new'><button className='btn btn-sm btn-outline-info m-3 py-2'>View Resume <i class="fas fa-download"/></button></a>
   )
 }
 
